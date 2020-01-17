da79e89c9e1b55b457abc68c12d01c28e83662f8
CarlosOrdonez <54687569+CarlosOrdonez@users.noreply.github.com>
2020-01-17 14:40:12 -0800

    Add files via upload

---
+++ stranger things.rb
@@ -0,0 +1,26 @@
+use_bpm 160
+live_loop:loop do
+  play:c4
+  play:e3
+  play:c2
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:b4
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:b4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:c4
+  sleep 0.5
+end
+
